ac223dda1664f8ab841beba644531ee4cdf4e0da
guusant <guiegucastro1@gmail.com>
2022-11-18 11:47:01 -0300

    kaskarias

---
+++ programa/programa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace juju_iluminada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] pontos = new int[5];
+            string[] times = new string[5];
+            for (int i = 0; i < times.Length; i++){
+                Console.WriteLine("Qual o nome do teu time?");
+                times[i] = Console.ReadLine();
+                Console.WriteLine();
+                Console.WriteLine("Quantos pontos seu time fez?");
+                pontos[i] = int.Parse(Console.ReadLine());
+                Console.Clear();
+            }
+            Console.WriteLine("! Fim :P !");
+            Console.ReadKey();
+        }
+    }
+}
